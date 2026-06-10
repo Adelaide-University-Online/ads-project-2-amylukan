8772ed79038481b0e72d3eade533009613a6ded1
amylukan <lukay008@mymail.unisa.edu.au>
2026-06-11 00:06:00 +0800

    Implement graph data structure using adjacency list

---
+++ src/main/java/Graph.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Graph {
+
+    private Map<String, List<String>> adjacencyList;
+    private Map<String, Integer> indegree;
+
+    public Graph() {
+        adjacencyList = new HashMap<>();
+        indegree = new HashMap<>();
+    }
+
+    public void addCourse(String course) {
+        adjacencyList.putIfAbsent(course, new ArrayList<>());
+        indegree.putIfAbsent(course, 0);
+    }
+
+    public void addPrerequisite(String prerequisite, String course) {
+        adjacencyList.get(prerequisite).add(course);
+        indegree.put(course, indegree.get(course) + 1);
+    }
+
+    public Map<String, List<String>> getAdjacencyList() {
+        return adjacencyList;
+    }
+
+    public Map<String, Integer> getIndegree() {
+        return indegree;
+    }
+}
